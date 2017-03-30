98a3183c6a2848add9b29c5f3a89d503df564103
Saurabh <saurabh.shendye@gmail.com>
2017-03-30 15:41:19 -0600

    Done with Mapper and job class for question 4

--- src/cs455/hadoop/Q4/urbanVsRuralMapper.java
+++ src/cs455/hadoop/Q4/urbanVsRuralMapper.java
@@ -1,11 +1,42 @@
-package cs455.hadoop.Q4;
-
-import org.apache.hadoop.mapreduce.Mapper;
-
-/**
+/*
  * Created by Saurabh on 3/30/2017.
  */
-public class urbanVsRuralMapper extends Mapper
+
+package cs455.hadoop.Q4;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+public class urbanVsRuralMapper extends Mapper<LongWritable, Text, Text, Text>
 {
+
+    public void map (LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException
+    {
+        String line = value.toString();
+        int partNo = Integer.parseInt(line.substring(24, 27));
+        if (partNo == 2)
+        {
+            int lineSummary = Integer.parseInt(line.substring(10, 12));
+            if(lineSummary == 100)
+            {
+                String state = line.substring(8, 9);
+
+                String urbanInUrban = line.substring(1821, 1829);
+                String urbanInNonUrban = line.substring(1830, 1838);
+                long totalUrban = Long.parseLong(urbanInUrban) + Long.parseLong(urbanInNonUrban);
+                String urban = Long.toString(totalUrban);
+
+                String rural = line.substring(1839, 1847);
+
+                context.write(new Text(state), new Text(urban + ":" +rural));
+            }
+        }
+
+    }
 
 }
