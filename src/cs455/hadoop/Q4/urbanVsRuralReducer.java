7bb01d0c74d8d52ff2e0d2e69dc16cef2010ab80
Saurabh <saurabh.shendye@gmail.com>
2017-03-30 15:45:24 -0600

    Started working on reducer class.. declared reduce method

--- src/cs455/hadoop/Q4/urbanVsRuralReducer.java
+++ src/cs455/hadoop/Q4/urbanVsRuralReducer.java
@@ -1,12 +1,23 @@
 package cs455.hadoop.Q4;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
 
 /**
  * Created by Saurabh on 3/30/2017.
  */
 
-public class urbanVsRuralReducer extends Reducer
+public class urbanVsRuralReducer extends Reducer<Text, Text, Text, Text>
 {
+    public void reduce (Text key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException
+    {
+        for (Text value: values)
+        {
+
+        }
+    }
 
 }
