7ab17c8941d4e769056bb9c42f19871198c0c2a7
Saurabh <saurabh.shendye@gmail.com>
2017-03-31 02:47:57 -0600

    Done with job class for Q6

---
+++ src/cs455/hadoop/Q6/rentMedianMapper.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Saurabh on 3/31/2017.
+ */
+
+package cs455.hadoop.Q6;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class rentMedianMapper extends Mapper
+{
+
+}
---
+++ src/cs455/hadoop/Q6/rentMedianReducer.java
@@ -0,0 +1,12 @@
+/*
+ * Created by Saurabh on 3/31/2017.
+ */
+
+package cs455.hadoop.Q6;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class rentMedianReducer extends Reducer
+{
+
+}
