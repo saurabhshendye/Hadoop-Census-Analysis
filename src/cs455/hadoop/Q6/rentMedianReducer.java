4317d5224014a31b1a98ee03b4e73f9bc39d4461
Saurabh <saurabh.shendye@gmail.com>
2017-03-31 13:22:19 -0600

    written reduce method for reducer of question6

--- src/cs455/hadoop/Q6/rentMedianReducer.java
+++ src/cs455/hadoop/Q6/rentMedianReducer.java
@@ -4,9 +4,21 @@
 
 package cs455.hadoop.Q6;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class rentMedianReducer extends Reducer
+import java.io.IOException;
+
+public class rentMedianReducer extends Reducer<Text, Text, Text, Text>
 {
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException
+    {
+        for (Text value: values)
+        {
+
+        }
+
+    }
 
 }
