c2ef8ccfe146089245f780fa980258680d7c3810
Saurabh <saurabh.shendye@gmail.com>
2017-03-30 19:25:25 -0600

    written the map function of mapper for question 5

--- src/cs455/hadoop/Q5/houseValueMedianMapper.java
+++ src/cs455/hadoop/Q5/houseValueMedianMapper.java
@@ -10,13 +10,46 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class houseValueMedianMapper extends Mapper
+public class houseValueMedianMapper extends Mapper<LongWritable, Text, Text, Text>
 {
 
-    public void map (LongWritable key, Text value, Context context)
+    public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException
     {
+        String line = value.toString();
+        int partNo = Integer.parseInt(line.substring(24, 27));
+        if (partNo == 2)
+        {
+            int lineSummary = Integer.parseInt(line.substring(10, 12));
+            if(lineSummary == 100)
+            {
+                String state = line.substring(8, 9);
 
+                String vLT15K = line.substring(2928, 2936);
+                String v15k = line.substring(2937, 2945);
+                String v20k = line.substring(2946, 2954);
+                String v25k = line.substring(2955, 2963);
+                String v30k = line.substring(2964, 2972);
+                String v35k = line.substring(2973, 2981);
+                String v40k = line.substring(2982, 2990);
+                String v45k = line.substring(2991, 2999);
+                String v50k = line.substring(3000, 3008 );
+                String v60k = line.substring(3009, 3017);
+                String v75k = line.substring(3018, 3026);
+                String v100k = line.substring(3027, 3035);
+                String v125k = line.substring(3036, 3044);
+                String v150k = line.substring(3045, 3053);
+                String v175k = line.substring(3054, 3062);
+                String v200k = line.substring(3063, 3071);
+                String v250k = line.substring(3072, 3080);
+                String v300k = line.substring(3081, 3089);
+                String v400k = line.substring(3090, 3098);
+                String v500k = line.substring(3099, 3107);
+
+
+//                context.write(new Text(state), new Text(owned + ":" + rented));
+            }
+        }
     }
 
 }
