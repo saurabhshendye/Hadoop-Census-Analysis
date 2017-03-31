8acff9cf0e466a0ac2f4bd6a0d66648cb26e8c40
Saurabh <saurabh.shendye@gmail.com>
2017-03-30 19:57:24 -0600

    Done with q5 mapper

--- src/cs455/hadoop/Q5/houseValueMedianMapper.java
+++ src/cs455/hadoop/Q5/houseValueMedianMapper.java
@@ -25,31 +25,60 @@ public class houseValueMedianMapper extends Mapper<LongWritable, Text, Text, Tex
             {
                 String state = line.substring(8, 9);
 
-                String vLT15K = line.substring(2928, 2936);
-                String v15k = line.substring(2937, 2945);
-                String v20k = line.substring(2946, 2954);
-                String v25k = line.substring(2955, 2963);
-                String v30k = line.substring(2964, 2972);
-                String v35k = line.substring(2973, 2981);
-                String v40k = line.substring(2982, 2990);
-                String v45k = line.substring(2991, 2999);
-                String v50k = line.substring(3000, 3008 );
-                String v60k = line.substring(3009, 3017);
-                String v75k = line.substring(3018, 3026);
-                String v100k = line.substring(3027, 3035);
-                String v125k = line.substring(3036, 3044);
-                String v150k = line.substring(3045, 3053);
-                String v175k = line.substring(3054, 3062);
-                String v200k = line.substring(3063, 3071);
-                String v250k = line.substring(3072, 3080);
-                String v300k = line.substring(3081, 3089);
-                String v400k = line.substring(3090, 3098);
-                String v500k = line.substring(3099, 3107);
+                String  summaryString = toString(line);
 
+                context.write(new Text(state), new Text(summaryString));
 
-//                context.write(new Text(state), new Text(owned + ":" + rented));
+//                String vLT15K = line.substring(2928, 2936);
+//                String v15k = line.substring(2937, 2945);
+//                String v20k = line.substring(2946, 2954);
+//                String v25k = line.substring(2955, 2963);
+//                String v30k = line.substring(2964, 2972);
+//                String v35k = line.substring(2973, 2981);
+//                String v40k = line.substring(2982, 2990);
+//                String v45k = line.substring(2991, 2999);
+//                String v50k = line.substring(3000, 3008 );
+//                String v60k = line.substring(3009, 3017);
+//                String v75k = line.substring(3018, 3026);
+//                String v100k = line.substring(3027, 3035);
+//                String v125k = line.substring(3036, 3044);
+//                String v150k = line.substring(3045, 3053);
+//                String v175k = line.substring(3054, 3062);
+//                String v200k = line.substring(3063, 3071);
+//                String v250k = line.substring(3072, 3080);
+//                String v300k = line.substring(3081, 3089);
+//                String v400k = line.substring(3090, 3098);
+//                String v500k = line.substring(3099, 3107);
             }
         }
+    }
+
+    private static String toString(String line)
+    {
+        String bigString = "";
+
+        bigString = bigString + line.substring(2928, 2936) + ":";
+        bigString = bigString + line.substring(2937, 2945) + ":";
+        bigString = bigString + line.substring(2946, 2954) + ":";
+        bigString = bigString + line.substring(2955, 2963) + ":";
+        bigString = bigString + line.substring(2964, 2972) + ":";
+        bigString = bigString + line.substring(2973, 2981) + ":";
+        bigString = bigString + line.substring(2982, 2990) + ":";
+        bigString = bigString + line.substring(2991, 2999) + ":";
+        bigString = bigString + line.substring(3000, 3008 ) + ":";
+        bigString = bigString + line.substring(3009, 3017) + ":";
+        bigString = bigString + line.substring(3018, 3026) + ":";
+        bigString = bigString + line.substring(3027, 3035) + ":";
+        bigString = bigString + line.substring(3036, 3044) + ":";
+        bigString = bigString + line.substring(3045, 3053) + ":";
+        bigString = bigString + line.substring(3054, 3062) + ":";
+        bigString = bigString + line.substring(3063, 3071) + ":";
+        bigString = bigString + line.substring(3072, 3080) + ":";
+        bigString = bigString + line.substring(3081, 3089) + ":";
+        bigString = bigString + line.substring(3090, 3098) + ":";
+        bigString = bigString + line.substring(3099, 3107);
+
+        return bigString;
     }
 
 }
