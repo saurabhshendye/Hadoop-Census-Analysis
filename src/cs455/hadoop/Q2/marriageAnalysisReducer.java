91511bf7712c6040fa6e7a3a172dcc9320708633
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 02:07:33 -0600

    Done with question 2 reducer

--- src/cs455/hadoop/Q2/marriageAnalysisReducer.java
+++ src/cs455/hadoop/Q2/marriageAnalysisReducer.java
@@ -4,9 +4,54 @@
 
 package cs455.hadoop.Q2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class marriageAnalysisReducer extends Reducer
+import java.io.IOException;
+
+public class marriageAnalysisReducer extends Reducer<Text, Text, Text, Text>
 {
+    private static long [] summary = new long[4];
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException
+    {
+        zeroInitialization();
+        for (Text value: values)
+        {
+            String [] byParts = value.toString().split(":");
+            addToArray(byParts);
+        }
+
+        String results = findStats();
+
+        context.write(key, new Text(results));
+    }
+
+    private static void zeroInitialization()
+    {
+        for (int i = 0; i < summary.length; i++)
+        {
+            summary[i] = 0;
+        }
+    }
+
+    private static void addToArray(String [] parts)
+    {
+        for (int i = 0; i < summary.length; i++)
+        {
+            summary[i] = summary[i] + Long.parseLong(parts[i]);
+        }
+    }
+
+    private static String findStats()
+    {
+        double malePercent = summary[2]/summary[0];
+        double femalePercent = summary[3]/summary[1];
+
+        String results = "Un-Married Male : " +  Double.toString(malePercent) + "\n"
+                        + "Un-Married Female: " + Double.toString(femalePercent);
+
+        return results;
+    }
 
 }
