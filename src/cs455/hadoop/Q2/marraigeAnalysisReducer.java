59d6b63d60ea2ae42e4e69128558be9678cc591a
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 00:47:19 -0600

    Created Classes for question 2

---
+++ src/cs455/hadoop/Q2/marraigeAnalysisReducer.java
@@ -0,0 +1,8 @@
+/*
+ * Created by Saurabh on 4/2/2017.
+ */
+
+package cs455.hadoop.Q2;
+
+public class marraigeAnalysisReducer {
+}
--- src/cs455/hadoop/Q7/avgPercentileMapper.java
+++ src/cs455/hadoop/Q7/avgPercentileMapper.java
@@ -1,7 +1,8 @@
-package cs455.hadoop.Q7;
-
-/**
+/*
  * Created by Saurabh on 4/2/2017.
  */
+
+package cs455.hadoop.Q7;
+
 public class avgPercentileMapper {
 }
