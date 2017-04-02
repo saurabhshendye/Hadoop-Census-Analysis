7f95df3b80781ba52446cb6ac498ed24107e6669
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 02:13:15 -0600

    Created map and reduce methods for question 7

--- src/cs455/hadoop/Q7/avgPercentileMapper.java
+++ src/cs455/hadoop/Q7/avgPercentileMapper.java
@@ -8,7 +8,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class avgPercentileMapper extends Mapper<LongWritable, Text, Text, Text>
 {
+
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException
+    {
+
+    }
 
 }
--- src/cs455/hadoop/Q7/avgPercentileReducer.java
+++ src/cs455/hadoop/Q7/avgPercentileReducer.java
@@ -7,7 +7,13 @@ package cs455.hadoop.Q7;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class avgPercentileReducer extends Reducer <Text, Text, Text, Text>
 {
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException
+    {
 
+    }
 }
