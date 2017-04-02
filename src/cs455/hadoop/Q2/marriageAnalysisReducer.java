bc3502f1eb5a79340df2946ebebf617c521128a9
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 01:39:34 -0600

    Fixed typos

---
+++ src/cs455/hadoop/Q2/marriageAnalysisReducer.java
@@ -0,0 +1,12 @@
+/*
+ * Created by Saurabh on 4/2/2017.
+ */
+
+package cs455.hadoop.Q2;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class marriageAnalysisReducer extends Reducer
+{
+
+}
