fc04b0a426b5988f47de3e5d963cbf4b9fa38397
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 15:28:16 -0600

    Done with question 7 reducer

--- src/cs455/hadoop/Q7/avgPercentileReducer.java
+++ src/cs455/hadoop/Q7/avgPercentileReducer.java
@@ -11,9 +11,59 @@ import java.io.IOException;
 
 public class avgPercentileReducer extends Reducer <Text, Text, Text, Text>
 {
+    private static long [] summary = new long[9];
     public void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException
     {
+        for (Text value: values)
+        {
+            String [] byParts = value.toString().split(":");
+            addToArray(byParts);
+        }
+    }
 
+    private void addToArray(String [] parts)
+    {
+        for (int i = 0; i < summary.length; i++)
+        {
+            summary[i] = summary[i] + Long.parseLong(parts[i]);
+        }
+    }
+
+    private static long summarySum()
+    {
+        long sum = 0;
+        for (long value: summary)
+        {
+            sum = sum + value;
+        }
+
+        return sum;
+    }
+
+    private static long findRoomCount()
+    {
+//        long [] count = new long[9];
+        long count = 0;
+        for (int i = 0; i < summary.length; i++)
+        {
+//            count[i] = summary[i] * (i+1);
+            count = count + (summary[i] * (i+1));
+        }
+
+        return count;
+    }
+
+
+    public void cleanup(Context context) throws IOException, InterruptedException
+    {
+        long total = summarySum();
+        long count = findRoomCount();
+
+        double averageRooms = count/total;
+
+        double percentile = 0.95 * averageRooms;
+
+        context.write(new Text("Percentile"), new Text(Double.toString(percentile)));
     }
 }
