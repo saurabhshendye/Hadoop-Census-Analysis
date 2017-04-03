f062ead7318621357c181ee814f728abe7a2b5b8
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 18:27:28 -0600

    Created map and reduce methods for question 3

--- src/cs455/hadoop/Q3/ageDistributionMapper.java
+++ src/cs455/hadoop/Q3/ageDistributionMapper.java
@@ -8,7 +8,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class ageDistributionMapper extends Mapper<LongWritable, Text, Text, Text>
 {
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException
+    {
 
+    }
 }
--- src/cs455/hadoop/Q3/ageDistributionReducer.java
+++ src/cs455/hadoop/Q3/ageDistributionReducer.java
@@ -7,7 +7,13 @@ package cs455.hadoop.Q3;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class ageDistributionReducer extends Reducer<Text, Text, Text, Text>
 {
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException
+    {
 
+    }
 }
