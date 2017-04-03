079151842dc03ae85993ffa4208881ccf1333157
Saurabh <saurabh.shendye@gmail.com>
2017-04-02 19:43:58 -0600

    Done with the reducer for question 3. All set and ready for testing.

--- src/cs455/hadoop/Q3/ageDistributionReducer.java
+++ src/cs455/hadoop/Q3/ageDistributionReducer.java
@@ -18,7 +18,49 @@ public class ageDistributionReducer extends Reducer<Text, Text, Text, Text>
     {
         for (Text value: values)
         {
-//            String [] byParts = value.toString().split()
+            String [] byParts = value.toString().split("/");
+            addToArray(byParts);
         }
+
+        String [] results = getResults();
+
+        context.write(key, new Text("Under 18 Male: " + results[0]
+                                    + "Males From 19 to 29: " + results[1]
+                                    + "Males From 30 to 39: " + results[2]
+                                    + "Under 18 Females: " + results[3]
+                                    + "Females 19 to 29: " + results[4]
+                                    + "Females 30 to 39: " + results[5]));
+
+    }
+
+    private static void addToArray(String [] parts)
+    {
+        for (int i = 0; i < summary.length; i++)
+        {
+            summary[i] = summary[i] + Long.parseLong(parts[i]);
+        }
+    }
+
+    private static String [] getResults()
+    {
+        String [] results = new String[6];
+
+        double under18MaleD = summary[0]/summary[6];
+        double from19to29MaleD = summary[1]/summary[6];
+        double from30to39MaleD = summary[2]/summary[6];
+
+        double under18FemaleD = summary[3]/summary[7];
+        double from19to29FemaleD = summary[4]/summary[7];
+        double from30to39FemaleD = summary[5]/summary[7];
+
+        results[0] = Double.toString(under18MaleD);
+        results[1] = Double.toString(from19to29MaleD);
+        results[2] = Double.toString(from30to39MaleD);
+
+        results[3] = Double.toString(under18FemaleD);
+        results[4] = Double.toString(from19to29FemaleD);
+        results[5] = Double.toString(from30to39FemaleD);
+
+        return results;
     }
 }
