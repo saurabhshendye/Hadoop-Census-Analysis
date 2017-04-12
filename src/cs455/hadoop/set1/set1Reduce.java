454c89aade5c2fd7721606a713c133387b0b0a0f
Saurabh <saurabh.shendye@gmail.com>
2017-04-11 21:52:02 -0600

    set1: Starting with the reducer class

--- src/cs455/hadoop/set1/set1Reduce.java
+++ src/cs455/hadoop/set1/set1Reduce.java
@@ -6,7 +6,14 @@ package cs455.hadoop.set1;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
 
+import java.io.IOException;
+
 public class set1Reduce extends Reducer<Text, Text, Text, Text>
 {
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException
+    {
+
+    }
 
 }
