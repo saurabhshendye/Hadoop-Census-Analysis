fcf8973f9efdb0e70c31c931816566c1f5be1986
Saurabh <saurabh.shendye@gmail.com>
2017-04-12 00:44:23 -0600

    Set-2.reduce: Q8 reduce done

--- src/cs455/hadoop/set2/set2Reduce.java
+++ src/cs455/hadoop/set2/set2Reduce.java
@@ -12,10 +12,42 @@ import java.io.IOException;
 
 public class set2Reduce extends Reducer<Text, Text, Text, Text>
 {
+    private static long [] q8summary = new long[2];
+    private static double q8percent = 0.0d;
+    private static String q8state;
+
     public void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException
     {
+        for (Text value: values)
+        {
+            String [] byParts = value.toString().split("/");
+            if (byParts[0].equals("part-1"))
+            {
+                String [] parts = byParts[1].split(":");
+                q8addToArray(parts);
+            }
+        }
 
+        if (q8percent < q8findPercent())
+        {
+            q8percent = q8findPercent();
+            q8state = key.toString();
+        }
     }
 
+//--------------Q8 Methods--------------//
+    private static void q8addToArray(String [] parts)
+    {
+        for (int i = 0; i < q8summary.length; i++)
+        {
+            q8summary[i] = q8summary[i] + Long.parseLong(parts[i]);
+        }
+    }
+
+    private static double q8findPercent()
+    {
+//        float percent = summary[1]/(summary[0] + summary[1]);
+        return q8summary[1] * 100.0d/(q8summary[0]);
+    }
 }
