d8e7bee7afd7077dc64b094410748ec3bbca9dae
Saurabh <saurabh.shendye@gmail.com>
2017-04-11 22:13:22 -0600

    set1.reducer: Done with the Q2 reducer

--- src/cs455/hadoop/set1/set1Reduce.java
+++ src/cs455/hadoop/set1/set1Reduce.java
@@ -10,10 +10,56 @@ import java.io.IOException;
 
 public class set1Reduce extends Reducer<Text, Text, Text, Text>
 {
+    private static long [] q2Summary = new long[4];
     public void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException
     {
+        zeroInitialization();
+        for (Text value: values)
+        {
+            String strValue = value.toString();
+            String [] byParts = strValue.split("/");
 
+            if (byParts[0].equals("part-1"))
+            {
+
+            }
+            else if (byParts[0].equals("part-2"))
+            {
+                String [] Q2 = byParts[1].split(":");
+                q2AddToArray(Q2);
+            }
+        }
+
+        String results = findStats();
+
+    }
+
+    private static String findStats()
+    {
+        double malePercent = q2Summary[2] * 100.0d/q2Summary[0];
+        double femalePercent = q2Summary[3] * 100.0d/q2Summary[1];
+
+        String results = "Un-Married Male : " +  Double.toString(malePercent) + "\n"
+                + "Un-Married Female: " + Double.toString(femalePercent);
+
+        return results;
+    }
+
+    private static void q2AddToArray(String [] parts)
+    {
+        for (int i = 0; i < q2Summary.length; i++)
+        {
+            q2Summary[i] = q2Summary[i] + Long.parseLong(parts[i]);
+        }
+    }
+
+    private static void zeroInitialization()
+    {
+        for (int i = 0; i < q2Summary.length; i++)
+        {
+            q2Summary[i] = 0;
+        }
     }
 
 }
