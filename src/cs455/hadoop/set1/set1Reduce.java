abccba488b7f04e4831bdc3b228c4a502e638dfe
Saurabh <saurabh.shendye@gmail.com>
2017-04-11 22:47:03 -0600

    set1.reducer: Done with the Q5 reducer

--- src/cs455/hadoop/set1/set1Reduce.java
+++ src/cs455/hadoop/set1/set1Reduce.java
@@ -7,11 +7,15 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class set1Reduce extends Reducer<Text, Text, Text, Text>
 {
     private static long [] q2Summary = new long[4];
     private static long [] q3summary = new long[8];
+    private static long [] q5summary = new long[20];
+    private static HashMap<String, String> q5valueMap = new HashMap<String, String>();
+
 
     public void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException
@@ -20,12 +24,15 @@ public class set1Reduce extends Reducer<Text, Text, Text, Text>
         long owned = 0;
         long rented = 0;
 
-        // for Question 2 and Question 3
+        // for Question 2 , Question 3 and Question 5
         zeroInitialization();
 
         // for Question 4
         long urban = 0;
         long rural = 0;
+
+        // for Question 5
+        q5setupHashMap();
 
         for (Text value: values)
         {
@@ -46,7 +53,9 @@ public class set1Reduce extends Reducer<Text, Text, Text, Text>
                 urban = urban + Long.parseLong(Q4[0]);
                 rural = rural + Long.parseLong(Q4[1]);
 
-
+                // Question 5
+                String [] Q5 = byParts[3].split(":");
+                q5addAllValues(Q5);
 
             }
             else if (byParts[0].equals("part-2"))
@@ -69,6 +78,16 @@ public class set1Reduce extends Reducer<Text, Text, Text, Text>
 
         // Question 3
         String [] q3Results = q3getResults();
+
+        // Question 5
+        long total = q5totalForCurrentKey();
+        long median = q5findMedian(total);
+
+        String index = q5findRange(median);
+
+        String range = q5valueMap.get(index);
+
+
 
     }
 
@@ -102,6 +121,11 @@ public class set1Reduce extends Reducer<Text, Text, Text, Text>
         for (int i = 0; i < q3summary.length; i++)
         {
             q3summary[i] = 0;
+        }
+
+        for (int i = 0; i < q5summary.length; i++)
+        {
+            q5summary[i] = 0;
         }
     }
 //-----------Q2 Methods end-----------//
@@ -138,4 +162,82 @@ public class set1Reduce extends Reducer<Text, Text, Text, Text>
         return results;
     }
 //-----------Q3 Methods end-----------//
+
+//-----------Q5 Methods-----------//
+
+    private static void q5addAllValues(String [] parts)
+    {
+        for (int i = 0; i < parts.length; i++)
+        {
+            long temp = Long.parseLong(parts[i]);
+            q5summary[i] = q5summary[i] + temp;
+        }
+    }
+
+    private static long q5totalForCurrentKey()
+    {
+        long total = 0;
+        for (long aSummary : q5summary) {
+            total = total + aSummary;
+        }
+
+        return total;
+    }
+
+    private static long q5findMedian(long total)
+    {
+        long median;
+        if (total % 2 == 0)
+        {
+            median = total /2;
+        }
+        else
+        {
+            median = 1 + (total/2);
+        }
+        return median;
+    }
+
+    private static String q5findRange(long median)
+    {
+        long sum = 0;
+        for (int i = 0; i<q5summary.length; i++)
+        {
+            if (sum< median)
+            {
+                sum = sum + q5summary[i];
+            }
+            else
+            {
+                return Integer.toString(i);
+            }
+
+        }
+        return Integer.toString(q5summary.length -1);
+    }
+
+    private static void q5setupHashMap()
+    {
+        q5valueMap.put("0", "Less than $15,000");
+        q5valueMap.put("1", "$15,000 - $19,999");
+        q5valueMap.put("2", "$20,000 - $24,999");
+        q5valueMap.put("3", "$25,000 - $29,999");
+        q5valueMap.put("4", "$30,000 - $34,999");
+        q5valueMap.put("5", "$35,000 - $39,999");
+        q5valueMap.put("6", "$40,000 - $44,999");
+        q5valueMap.put("7", "$45,000 - $49,999");
+        q5valueMap.put("8", "$50,000 - $59,999");
+        q5valueMap.put("9", "$60,000 - $74,999");
+        q5valueMap.put("10", "$75,000 - $99,999");
+        q5valueMap.put("11", "$100,000 - $124,999");
+        q5valueMap.put("12", "$125,000 - $149,999");
+        q5valueMap.put("13", "$150,000 - $174,999");
+        q5valueMap.put("14", "$175,000 - $199,999");
+        q5valueMap.put("15", "$200,000 - $249,999");
+        q5valueMap.put("16", "$250,000 - $299,999");
+        q5valueMap.put("17", "$300,000 - $399,999");
+        q5valueMap.put("18", "$400,000 - $499,999");
+        q5valueMap.put("19", "$500,000 or more");
+    }
+
 }
