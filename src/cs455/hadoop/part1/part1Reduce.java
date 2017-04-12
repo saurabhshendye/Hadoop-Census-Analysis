fe6b696eeab5cd08b6a2b18cd30e380f33b530fe
Saurabh <saurabh.shendye@gmail.com>
2017-04-12 00:07:01 -0600

    part1.job : created

--- src/cs455/hadoop/part1/part1Reduce.java
+++ src/cs455/hadoop/part1/part1Reduce.java
@@ -1,7 +1,9 @@
 package cs455.hadoop.part1;
 
+import org.apache.hadoop.mapreduce.Reducer;
+
 /**
  * Created by Saurabh on 4/11/2017.
  */
-public class part1Reduce {
+public class part1Reduce extends Reducer {
 }
